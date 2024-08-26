0ef72fe7c2daa0342ca3db59983a51dcb956d3b2
AEndrix <redegalli.andrea03@gmail.com>
2024-08-26 02:17:35 +0200

    fine user info menu

--- AE-Wallet/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ AE-Wallet/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,28 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { MaterialModule } from '../../modules/material.module';
 import { Router } from '@angular/router';
 import { routes } from '../../../app.routes';
 import { UserInfoComponent } from '../user-info/user-info.component';
 import { UserDto } from '../../models/user.model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MaterialModule, UserInfoComponent],
+  imports: [MaterialModule, UserInfoComponent, NgIf],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
-  homeRoute: string = 'home';
-  walletsRoute: string = 'wallets';
-
-  user: UserDto | null = {
+  @Input() user: UserDto | null = null; /*{
     id: 1,
     name: 'Andrea',
     surname: 'Redegalli',
     email: '',
-  };
+  };*/
+
+  homeRoute: string = 'home';
+  walletsRoute: string = 'wallets';
 
   constructor(private router: Router) {}
 
