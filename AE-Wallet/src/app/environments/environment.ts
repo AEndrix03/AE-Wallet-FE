42b47aa31ef9efec3b4a3bd2619a6b65f614044c
AEndrix <redegalli.andrea03@gmail.com>
2024-09-14 21:54:12 +0200

    environment

--- AE-Wallet/src/app/environments/environment.ts
+++ AE-Wallet/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://217.160.248.228:8080/api',
   featureFlag: false,
   debugMode: false,
 };
