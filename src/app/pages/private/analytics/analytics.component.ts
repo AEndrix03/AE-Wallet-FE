ac901a1d2fa55d955669e822bbd8cb5477f6824e
AEndrix <redegalli.andrea03@gmail.com>
2025-09-20 03:56:18 +0200

    idea analytics

--- src/app/pages/private/analytics/analytics.component.ts
+++ src/app/pages/private/analytics/analytics.component.ts
@@ -7,3 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './analytics.component.css',
 })
 export class AnalyticsComponent {}
+
+/*
+Idee:
+
+- Grafici di categoria
+- Grafiici di andamento spese/entrate
+- Grafici di trend mensile/annuale
+- Torta di ripartizione spese/entrate + torta di ripartizione desiderata
+- Valutazioni e suggerimenti basati sui dati delle torte: si calcoala la differenza tra spese effettive e desiderate e si forniscono consigli su come migliorare la gestione finanziaria.
+
+ */
